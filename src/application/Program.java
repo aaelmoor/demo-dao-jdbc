8b3c86e1814b3aa0c3d9ced39222986104b9ff71
Alexandre Abdu El-moor <alexandre.elmoor@gmail.com>
2022-02-02 15:28:25 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,10 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 
-//		Department obj = new Department(1, "Books");
-//		Seller seller = new Seller(21,"José das Couves", "dascouves@gmail.com", new Date(), 3000.00, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
